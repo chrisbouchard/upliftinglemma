40b7ebaa10d96549d74cdd70302b505a8f0ef7f2
Chris Bouchard <cbouchard@leadingageny.org>
2014-06-15 05:18:20 -0400

    Set the load event on the window.

--- assets/js/anchor.js
+++ assets/js/anchor.js
@@ -28,7 +28,7 @@
         $(window).scrollTop(offset - navHeight);
     }
 
-    $(document.body).on('click', 'a', function (event) {
+    $(document).on('click', 'a', function (event) {
         var hash;
         var href = $(this).attr('href');
 
@@ -39,7 +39,7 @@
         }
     });
 
-    $(document.body).on('load', function (event) {
+    $(window).on('load', function (event) {
         if (location.hash.length !== 0) {
             scrollForHashWithNavbar(location.hash);
         }
