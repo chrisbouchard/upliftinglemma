056ddaa81c6a4f0869bd8918b09617bdeda53482
Chris Bouchard <cbouchard@leadingageny.org>
2014-06-21 23:31:03 -0400

    Add more logging.

--- assets/js/google.js
+++ assets/js/google.js
@@ -1,26 +1,29 @@
 (function () {
     var Google = angular.module('UpliftingLemma.Google', []);
 
-    Google.controller(['$scope', '$window', function ($scope, $window) {
-        $scope.on('event:google-plus-signin-success',
-            function (event, authResult) {
-                $http.post('/signin/store', {
-                    'CSRFToken': $window.CSRFToken,
-                    'GoogleData': authResult.code
-                }).success(function (data, status, headers, config) {
+    Google.controller('cbGPlusController', ['$scope', '$window',
+        function ($scope, $window) {
+            $scope.on('event:google-plus-signin-success',
+                function (event, authResult) {
                     console.log('Signin success!');
-                    gapi.auth.signOut();
-                }).error(function (data, status, headers, config) {
-                    console.log('Signin failed!');
-                    gapi.auth.signOut();
-                });
-            }
-        );
 
-        $scope.on('event:google-plus-signin-failure',
-            function (event, authResult) {
-            }
-        );
-    }]);
+                    $http.post('/signin/store', {
+                        'CSRFToken': $window.CSRFToken,
+                        'GoogleData': authResult.code
+                    }).success(function (data, status, headers, config) {
+                        console.log('Store success!');
+                    }).error(function (data, status, headers, config) {
+                        console.log('Store fail!');
+                    });
+                }
+            );
+
+            $scope.on('event:google-plus-signin-failure',
+                function (event, authResult) {
+                    console.log('Signin fail!');
+                }
+            );
+        }
+    ]);
 })();
 
