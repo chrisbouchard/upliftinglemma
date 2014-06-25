d0ca2967e05200ef2c94fe3019fb6f035d9fd269
Chris Bouchard <cbouchard@leadingageny.org>
2014-06-24 22:52:02 -0400

    Use _.chain correctly.

--- assets/js/google.js
+++ assets/js/google.js
@@ -50,7 +50,7 @@
                         var reqs = [authResult.code, attrs.serverCallback];
 
                         // If they are...
-                        if (_(reqs).map(angular.isDefined).all().value()) {
+                        if (_.chain(reqs).map(angular.isDefined).each().value()) {
                             var postData = $.param(angular.extend(
                                 {'code': authResult.code},
                                 scope.$eval(attrs.serverData)
